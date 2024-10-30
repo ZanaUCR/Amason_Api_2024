acb1dc230c7a9c1b6667f221e351d84c829a3505
JulioPicado <picadorodriguezj@gmail.com>
2024-10-29 20:19:38 -0600

    clave primaria para el producto

--- app/Models/product.php
+++ app/Models/product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
-{
+{protected $primaryKey = 'product_id';
     use HasFactory;
 
     // Agrega los atributos que se pueden llenar de forma masiva
